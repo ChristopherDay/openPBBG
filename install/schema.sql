471053cb711c1395b6381fcaed52772a3772bc0b
Christopher Day <chris@cdcoding.com>
2023-05-30 18:16:58 +0100

    added shoutbox SQL

--- install/schema.sql
+++ install/schema.sql
@@ -137,3 +137,11 @@ CREATE TABLE `itemMeta` (
   `IM_value` TEXT, 
   PRIMARY KEY(`IM_item`, `IM_meta`)
 );
+
+CREATE TABLE IF NOT EXISTS `chat` (
+  `CH_id` int(11) NOT NULL AUTO_INCREMENT,
+  `CH_user` int(11) NOT NULL,
+  `CH_time` int(11) NOT NULL,
+  `CH_text` varchar(128) NOT NULL,
+  PRIMARY KEY (`CH_id`)
+) DEFAULT CHARSET=utf8;
