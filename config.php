d6228ed0154f42b8a3dbabf6f778940c637c2900
agscs <ags_cs4@yahoo.com>
2024-12-11 08:52:47 +0100

    Clean Config

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $config["debug"] = true;
 
 $config["db"] = array(
     "host" => "localhost", 
-    "database" => "glv2_2.6",
-    "user" => "root",
-    "pass" => "123"
+    "database" => "",
+    "user" => "",
+    "pass" => ""
 );
